8f6e8ef4d2485f6a2108bde0c84abfadb4eddb99
Harshit Mehta <harshit.mehta@mongodb.com>
2022-07-21 22:16:53 +0100

    Form design, submit, and redirect

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -6,12 +6,12 @@ export default async function handler(req, res) {
     const { db } = await connectToDatabase();
   
     if (req.method === "POST") {
-        const data = req.body; 
-           
-        const result = await db.collection("testform").insertOne(data);
-        console.log(result);
+        const data = req.body;  
+        const result = await db.collection("event").insertOne(data);
+
+        res.status(401).send('<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url=/event"></head>Event Submitted Successful</html>');
         
-        res.status(201).json({ message: "Data inserted successfully!" });
+        //res.status(201).json({ message: "Event inserted successfully!" }).redirect(307, '/event');
 
         }
 }
--- pages/form.js
+++ pages/form.js
@@ -1,203 +1,359 @@
-import * as React from 'react';
+import * as React from 'react'; 
 
 export default function FormPropsTextFields() {
   return (
-    <body>
-      <div className="" />
-      <div className="title"></div>
-      <div className="content">
-        <form action="#" />
-        <div className="user-details">
-          <div className="input-box">
-            <span className="details">Full Name</span>
-            <input type="text" placeholder="Enter organizer name" required />
-          </div>
-          <div className="input-box">
-            <span className="details">Email</span>
-            <input type="text" placeholder="Enter organizer email" required />
-          </div>
-          <div className="input-box">
-            <span className="details">City</span>
-            <input type="text" placeholder="Enter City" required />
-          </div>
-          <div className="input-box">
-            <span className="details">Country</span>
-            <input type="text" placeholder="Enter Country" required />
-          </div>
-          <div className="input-box">
-            <span className="details">Region</span>
-            <input type="text" placeholder="Enter Region" required />
-          </div>
-          <div className="input-box">
-            <span className="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required />
-          </div>
-        </div>
-        <div className="role-details">
-          <input type="radio" name="role" id="dot-1" />
-          <input type="radio" name="role" id="dot-2" />
-          <span className="gender-title">Community/Staff</span>
-          <div className="category">
-            <label for="dot-1">
-              <span className="dot one"></span>
-              <span className="gender" value="Community">Community</span>
-            </label>
-            <label for="dot-2">
-              <span className="dot two"></span>
-              <span className="gender" value="Staff">Staff</span>
-            </label>
-          </div>
-        </div>
-        <div className="button">
-          <input type="submit" value="Register" />
-        </div>
-      </div>
-      <style jsx>{`
+    <main>
+      <img src="./mug.svg" alt="Mugongo" width="410" height="120" />
+    
+    <div class="row">
+    <div class="col-md-12">
+      <form action="/api/submit" method="post">
+        <h1> Log an Event </h1>
+        
+        <fieldset>
           
-          @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
-*{
-  margin: 0;
-  box-sizing: border-box;
-  font-family: 'Poppins',sans-serif;
-}
-body{
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 10px;
-  background: linear-gradient(135deg, #71b7e6, #9b59b6);
-}
-.container{
-  max-width: 700px;
-  width: 100%;
-  background-color: #fff;
-  padding: 25px 30px;
-  border-radius: 5px;
-  box-shadow: 0 5px 10px rgba(0,0,0,0.15);
-}
-.container .title{
-  font-size: 25px;
-  font-weight: 500;
-  position: relative;
-}
-.container .title::before{
-  content: "";
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  height: 3px;
-  width: 30px;
-  border-radius: 5px;
-  background: linear-gradient(135deg, #71b7e6, #9b59b6);
-}
-.content form .user-details{
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin: 20px 0 12px 0;
-}
-form .user-details .input-box{
-  margin-bottom: 15px;
-  width: calc(100% / 2 - 30px);
-}
-form .input-box span.details{
-  display: block;
-  font-weight: 500;
-  margin-bottom: 5px;
-}
-.user-details .input-box input{
-  height: 45px;
-  width: 100%;
-  outline: none;
-  font-size: 16px;
-  border-radius: 5px;
-  padding-left: 15px;
-  border: 1px solid #ccc;
-  border-bottom-width: 2px;
-  transition: all 0.3s ease;
-}
-.user-details .input-box input:focus,
-.user-details .input-box input:valid{
-  border-color: #9b59b6;
-}
- form .gender-details .gender-title{
-  font-size: 20px;
-  font-weight: 500;
- }
- form .category{
-   display: flex;
-   width: 80%;
-   margin: 14px 0 ;
-   justify-content: space-between;
- }
- form .category label{
-   display: flex;
-   align-items: center;
-   cursor: pointer;
- }
- form .category label .dot{
-  height: 18px;
-  width: 18px;
-  border-radius: 50%;
-  margin-right: 10px;
-  background: #d9d9d9;
-  border: 5px solid transparent;
-  transition: all 0.3s ease;
-}
- #dot-1:checked ~ .category label .one,
- #dot-2:checked ~ .category label .two,
- #dot-3:checked ~ .category label .three{
-   background: #9b59b6;
-   border-color: #d9d9d9;
- }
- form input[type="radio"]{
-   display: none;
- }
- form .button{
-   height: 45px;
-   margin: 35px 0
- }
- form .button input{
-   height: 100%;
-   width: 100%;
-   border-radius: 5px;
-   border: none;
-   color: #fff;
-   font-size: 18px;
-   font-weight: 500;
-   letter-spacing: 1px;
-   cursor: pointer;
-   transition: all 0.3s ease;
-   background: linear-gradient(135deg, #71b7e6, #9b59b6);
- }
- form .button input:hover{
-  /* transform: scale(0.99); */
-  background: linear-gradient(-135deg, #71b7e6, #9b59b6);
-  }
- @media(max-width: 584px){
- .container{
-  max-width: 100%;
-}
-form .user-details .input-box{
-    margin-bottom: 15px;
-    width: 100%;
-  }
-  form .category{
-    width: 100%;
-  }
-  .content form .user-details{
-    max-height: 300px;
-    overflow-y: scroll;
-  }
-  .user-details::-webkit-scrollbar{
-    width: 5px;
-  }
-  }
-  @media(max-width: 459px){
-  .container .content .category{
-    flex-direction: column;
-  }`}</style>
-    </body>
+          <legend><span>1</span> Basic Info</legend>
+        
+          <label for="group">Group:</label>
+          <input type="text" id="group" name="group"  />
+        
+          <label for="date">Date:</label>
+          <input type="date" id="date" name="date"  />
+       
+          <label for="title">Title:</label>
+          <input type="text" id="title" name="title" />
+
+          <label for="topics">Topics:</label>
+          <input type="text" id="topics" name="topics" />
+        
+          <label for="event">Type:</label> <br />
+          <select id="event" name="event">
+              <option value="MUG">MUG Event</option>
+              <option value="University">University Event</option>
+              <option value="Third Party Event">Third Party Event</option>
+          </select>
+
+        </fieldset>
+        
+        <fieldset>  
+        
+          <legend><span>2</span> Details</legend>
+          
+          <label for="attendees">Attendees:</label>
+          <input type="text" id="attendees" name="attendees"  />
+
+          <label for="duration">Duration:</label>
+          <input type="text" id="duration" name="duration"  />
+
+          <label for="budget">Budget:</label>
+          <input type="text" id="budget" name="budget"  />
+
+          <label for="primaryLeader">Primary Leader:</label>
+          <input type="text" id="primaryLeader" name="primaryLeader"  />
+          
+         </fieldset>
+       
+        <button type="submit">Submit</button>
+        
+       </form>
+       </div>
+       </div>
+      
+      <div className="grid">
+        <a href="http://localhost:3000/" className="card">
+          <h3>Home &rarr;</h3>
+          <p>Learn about MUG's.</p>
+        </a>
+
+        <a href="http://localhost:3000/event" className="card">
+          <h3>Events &rarr;</h3>
+          <p>Find about Events.</p>
+        </a>
+
+        <a href="http://localhost:3000/organizer" className="card">
+          <h3>Organizers &rarr;</h3>
+          <p>Learn about Organizers.</p>
+        </a>
+
+        <a href="http://localhost:3000/form" className="card">
+          <h3>Insert Data &rarr;</h3>
+          <p>Save data into MongoDB.</p>
+        </a>
+      </div>
+      <footer>
+        <a
+          href="https://mongodb.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/mug.svg" alt="MongoDB Logo" className="logo" />
+        </a>
+      </footer>
+
+
+      <style jsx>{`
+          *, *:before, *:after {
+            -moz-box-sizing: border-box;
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+          }
+          
+          form {
+            font-family: 'Nunito', sans-serif;
+            color: #384047;
+            max-width: 800px;
+            margin: 10px auto;
+            padding: 10px 20px;
+            background: #f4f7f8;
+            border-radius: 8px;
+          }
+          
+          h1 {
+            margin: 0 0 30px 0;
+            text-align: center;
+          }
+          
+          input[type="text"],
+          input[type="password"],
+          input[type="date"],
+          input[type="datetime"],
+          input[type="email"],
+          input[type="number"],
+          input[type="search"],
+          input[type="tel"],
+          input[type="time"],
+          input[type="url"],
+          textarea,
+          select {
+            background: rgba(255,255,255,0.1);
+            border: none;
+            font-size: 16px;
+            height: auto;
+            margin: 0;
+            outline: 0;
+            padding: 15px;
+            width: 100%;
+            background-color: #e8eeef;
+            color: #8a97a0;
+            box-shadow: 0 1px 0 rgba(0,0,0,0.03) inset;
+            margin-bottom: 30px;
+          }
+          
+          input[type="radio"],
+          input[type="checkbox"] {
+            margin: 0 4px 8px 0;
+          }
+          
+          select {
+            padding: 6px;
+            height: 32px;
+            border-radius: 2px;
+          }
+          
+          button {
+            padding: 19px 39px 18px 39px;
+            color: #FFF;
+            background-color: #4bc970;
+            font-size: 18px;
+            text-align: center;
+            font-style: normal;
+            border-radius: 5px;
+            width: 100%;
+            border: 1px solid #3ac162;
+            border-width: 1px 1px 3px;
+            box-shadow: 0 -1px 0 rgba(255,255,255,0.1) inset;
+            margin-bottom: 10px;
+            cursor: pointer;
+          }
+          
+          fieldset {
+            margin-bottom: 30px;
+            border: none;
+          }
+          
+          legend {
+            font-size: 1.4em;
+            margin-bottom: 10px;
+          }
+          
+          label {
+            display: block;
+            margin-bottom: 8px;
+            font-size: 16px;
+          }
+          
+          label.light {
+            font-weight: 300;
+            display: inline;
+          }
+          
+          span {
+            background-color: #5fcf80;
+            color: #fff;
+            height: 30px;
+            width: 30px;
+            display: inline-block;
+            font-size: 0.8em;
+            margin-right: 4px;
+            line-height: 30px;
+            text-align: center;
+            text-shadow: 0 1px 0 rgba(255,255,255,0.2);
+            border-radius: 100%;
+          }
+          
+          @media screen and (min-width: 480px) {
+            form {
+              min-width: 800px;
+            }
+      
+          }
+
+
+
+
+        main {
+          padding: 1rem 1rem 1rem 1rem;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          background-color: #d1e0d5;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #ffffff;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .subtitle {
+          font-size: 2rem;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        code {
+          background: #ffffff;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: nowrap;
+          min-width: 500px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+          flex-basis: 100%;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: row;
+          }
+        }
+
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+
+     
+    </main>
   );
 }
